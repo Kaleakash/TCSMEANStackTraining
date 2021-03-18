03f1b7998ead2536c6db1347ff45cffddf2e3ad7
Kaleakash <akash300383@gmail.com>
2021-03-18 21:13:43 +0530

    Data types

---
+++ TypeScript/data_type.js
@@ -0,0 +1,13 @@
+var a = 100;
+var b = 100.10;
+var fname = "Ajay Kumar";
+var res = true;
+var n = 100; // base upon the value it become that type. 
+var id;
+var stdName;
+var age;
+//n="Ramesh"    Error 
+//a=false;      Error 
+id = 123;
+stdName = "Mahesh KUmar";
+age = 24;
---
+++ TypeScript/data_type.ts
@@ -0,0 +1,16 @@
+let a:number=100;
+let b:number=100.10;
+let fname:string="Ajay Kumar";
+let res:boolean = true;
+let n=100;      // base upon the value it become that type. 
+let id:number;
+let stdName:string;
+let age:number;
+let msg:any;        // any type
+//n="Ramesh"    Error 
+//a=false;      Error 
+id=123;
+stdName="Mahesh KUmar";
+age = 24;
+msg=100;
+msg="Ravi Kumar";
