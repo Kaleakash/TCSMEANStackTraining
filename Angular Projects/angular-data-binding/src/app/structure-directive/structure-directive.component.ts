df87424c84ff7d2a9e5e0b5d930cf925587a6c96
Kaleakash <akash300383@gmail.com>
2021-03-23 22:29:38 +0530

    employee details

--- Angular Projects/angular-data-binding/src/app/structure-directive/structure-directive.component.ts
+++ Angular Projects/angular-data-binding/src/app/structure-directive/structure-directive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-structure-directive',
@@ -10,6 +11,9 @@ export class StructureDirectiveComponent implements OnInit {
   b1:string="show"
   stdNames:Array<string>=["Raj","Seeta","Reeta","Meeta","Keeta","Teeta"];
   skillNames:Array<String>=new Array();
+  
+  emp:Employee=new Employee(100,"Ravi",12000);
+
   constructor() { }
 
   ngOnInit(): void {
