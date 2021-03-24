99abb936e5ad0d3e6f709581fc76ada5dbbeddbb
Kaleakash <akash300383@gmail.com>
2021-03-24 22:18:18 +0530

    Angular Routing

--- Angular Projects/angular-routing/src/app/app-routing.module.ts
+++ Angular Projects/angular-routing/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+// API hold all routing path details. 
+const routes: Routes = [
+  {path:"\aboutus",component:AboutusComponent},
+  {path:"\contactus",component:ContactusComponent},
+  {path:"\login",component:LoginComponent},
+  {path:"\home",component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular Projects/angular-routing/src/app/login/login.component.ts
+++ Angular Projects/angular-routing/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public router:Router) { }   //DI : Dependency Injection 
+                                          // creating object withtout new kewyord 
   ngOnInit(): void {
+  }
+  homePage(){
+    this.router.navigate(["home"]);
   }
 
 }
