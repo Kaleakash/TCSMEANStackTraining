324478f7ed322fad35f29a6b2886ad10ff150b49
Kaleakash <akash300383@gmail.com>
2021-03-24 21:42:17 +0530

    Angular Forms

--- Angular Projects/angular-routing/src/app/app.module.ts
+++ Angular Projects/angular-routing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
