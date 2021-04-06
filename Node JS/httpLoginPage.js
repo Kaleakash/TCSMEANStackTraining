38f6208f3694afb338333b9c1a29e49e071a4c30
kaleakash <akash300383@gmail.com>
2021-04-06 20:51:32 +0530

    http with login page

--- Node JS/httpLoginPage.js
+++ Node JS/httpLoginPage.js
@@ -1,7 +1,22 @@
 let http = require("http");
 let url = require("url");
 let port = 9999;
+let loginInfo = `
+        <form action="/login" method="get">
+        <label>UserName</label>
+        <input type="text" name="user"/><br/>
+        <label>Password</label>
+        <input type="password" name="pass"/><br/>
+        <input type="submit" value="Sign In!"/>
+        <input type="reset" value="reset"/>
+    </form>
+`
 let server = http.createServer((req,res)=> {
+    console.log(req.url)
+    if(req.url=="/"){
+    res.setHeader("content-type","text/html");  // by default data consider as a html 
+    res.end(loginInfo);
+    }else {
     var data = url.parse(req.url,true).query;
     if(data.user=="Ajay" && data.pass=="Kumar"){
         res.write("Successfully Login!");
@@ -9,5 +24,6 @@ let server = http.createServer((req,res)=> {
         res.write("Failure try once again");
     }
     res.end();
+    }
 })
 server.listen(port,()=>console.log(`running on port num ${port}`));
