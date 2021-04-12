684da4d8c08fa73b1d275bc5b2d0090a181403b4
kaleakash <akash300383@gmail.com>
2021-04-12 22:27:25 +0530

    socket io

--- Node JS/WebSocket/server.js
+++ Node JS/WebSocket/server.js
@@ -4,9 +4,11 @@ var ws = require("express-ws")(app);
 app.get("/",(req,res)=> {
     res.sendFile(__dirname+"/index.html");
 })
-
 app.ws("/",(socket,res)=> {
-    socket.send("Message from Server.....");
+    socket.send("Message from Server....."); 
+})
+app.ws("/open",(data)=> {
+    console.log(data.toString())
 })
 
 app.listen(9090,()=>console.log("WebSocket server running on port number 9090 "))
---
+++ Node JS/socket.io/server.js
@@ -0,0 +1,16 @@
+let app = require("express")();
+let http = require("http").Server(app);   // to load the library we have run port number using hhtp module 
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=> {
+    console.log("Client connected to application.....");
+
+    socket.on("chat message",(msg)=> {
+        console.log(msg);
+    })
+})
+http.listen(9090,()=>console.log('server running on port number 9090'));
