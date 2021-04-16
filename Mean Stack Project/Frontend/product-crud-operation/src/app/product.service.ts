19a626e58665995e86738f464fa7f59b6a244083
kaleakash <akash300383@gmail.com>
2021-04-16 22:26:57 +0530

    done

--- Mean Stack Project/Frontend/product-crud-operation/src/app/product.service.ts
+++ Mean Stack Project/Frontend/product-crud-operation/src/app/product.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Product } from './model.product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
   constructor(public http:HttpClient) { }
 //post method 1st parameter url and 2nd parameter json data. 
-
   storeProductDetailsInfo(productRef:any){
     this.http.post("http://localhost:9090/product/storeProductDetails",productRef).
     subscribe(result=>console.log(result),error=>console.log(error));
   }
+  retrieveAllProductDetails():Observable<Product[]>{
+     return this.http.get<Product[]>("http://localhost:9090/product/allProductDetails")
+  }
+
+
 }
